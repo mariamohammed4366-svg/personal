52bd9d5ef3b67495cc49a4a13b4abf9a75e6f284
mariamohammed4366-svg <mariamohammed4366@gmail.com>
2025-12-24 22:18:08 +0300

    Delete scrip.js

--- scrip.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("toggle-theme");
-
-btn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-
-  btn.textContent = document.body.classList.contains("dark")
-    ? "Light Mode"
-    : "Dark Mode";
-});
